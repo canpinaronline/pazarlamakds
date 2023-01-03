7c266d74a35ac18d75b8433d9539252a56f9397e
canpinaronline <64422728+canpinaronline@users.noreply.github.com>
2023-01-04 01:51:22 +0300

    Delete yordam.php

--- yordam.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('baglanti.php');
-include('veri.php');?>
-
-<?php
-rsort($metrekare_verimlilikleri);
-print_r($metrekare_verimlilikleri);
-$test = array_search("57865", $metrekare_verimlilikleri);
-echo $test;
